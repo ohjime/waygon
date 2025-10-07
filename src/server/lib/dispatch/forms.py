f6ce99eaf450a8047b841d9adbbfbde561fa20f7
Osman Jime <ohjime@icloud.com>
2025-10-07 12:46:37 -0600

    Django Partials and HTMX example

--- src/server/lib/dispatch/forms.py
+++ src/server/lib/dispatch/forms.py
@@ -5,4 +5,4 @@ from core.models import Account
 class AccountForm(forms.ModelForm):
     class Meta:
         model = Account
-        fields = ("first_name", "last_name", "email", "phone", "avatar")
+        fields = ("uid", "first_name", "last_name", "email", "phone", "avatar")
